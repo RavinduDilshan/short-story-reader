317d9ab11160eb97be19632a4b53c22ca16c2694
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2025-09-02 22:15:58 +0530

    feat: update StoryDetails to handle favorite state and improve UI responsiveness

--- lib/story_details.dart
+++ lib/story_details.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:provider/provider.dart';
 import 'package:sinhala_short_stories/helpers/enums.dart';
+import 'package:sinhala_short_stories/providers/favorite_story_provider.dart';
 import 'package:sinhala_short_stories/providers/story_details_provider.dart';
 import 'package:flutter/material.dart';
 
@@ -27,7 +28,7 @@ class _StoryDetailsState extends State<StoryDetails> {
 
   @override
   Widget build(BuildContext context) {
-    // isFavorite = Provider.of<FavoriteStories>(context).checkFavorite((post?.id ?? -1).toString());
+    isFavorite = Provider.of<FavoriteStories>(context).checkFavorite((widget.storyId).toString());
 
     const snackBar = SnackBar(
       duration: Duration(seconds: 2),
@@ -72,109 +73,112 @@ class _StoryDetailsState extends State<StoryDetails> {
                     ),
         );
 
-    return Scaffold(
-      /*  floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.white,
-        onPressed: () async {
-          //toggle favorite state
-          if (!isFavorite) {
-            await Provider.of<FavoriteStories>(context, listen: false).addFavorite(
-                post?.title ?? '',
-                (post?.id ?? -1).toString(),
-                post?.author ?? '',
-                post?.image ?? '');
-            if (mounted) {
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    return Consumer<StoryDetailsProvider>(
+      builder: (context, provider, child) {
+        return Scaffold(
+         floatingActionButton:provider.loadingState == LoadingState.loading || provider.loadingState == LoadingState.idle || provider.loadingState == LoadingState.error ? null:  FloatingActionButton(
+          backgroundColor: Colors.white,
+          onPressed: () async {
+            //toggle favorite state
+            if (!isFavorite) {
+              await Provider.of<FavoriteStories>(context, listen: false).addFavorite(
+                  provider.story?.title ?? '',
+                  (provider.story?.id ?? -1).toString(),
+                  provider.story?.author ?? '',
+                  provider.story?.image ?? '');
+              if (mounted) {
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              }
+            } else {
+              //remove from favorites
+              await Provider.of<FavoriteStories>(context, listen: false).deleteFavorite(
+                (provider.story?.id ?? -1).toString(),
+              );
             }
-          } else {
-            //remove from favorites
-            await Provider.of<FavoriteStories>(context, listen: false).deleteFavorite(
-              (post?.id ?? -1).toString(),
-            );
-          }
-        },
-        child: isFavorite
-            ? Icon(
-                Icons.favorite,
-                color: Color(0xff5b5858),
-                size: 50,
-              )
-            : Icon(
-                Icons.favorite_border_outlined,
-                color: Color(0xff5b5858),
-                size: 50,
-              ),
-      ), */
-      body: Consumer<StoryDetailsProvider>(
-        builder: (context, provider, child) {
-          return CustomScrollView(
-            slivers: [
-              SliverAppBar(
-                leading: IconButton(
-                  icon: Icon(
-                    Icons.arrow_back,
-                    color: Color(0xff5b5858),
-                  ),
-                  onPressed: () => Navigator.of(context).pop(),
+          },
+          child: isFavorite
+              ? Icon(
+                  Icons.favorite,
+                  color: Color(0xff5b5858),
+                  size: 50,
+                )
+              : Icon(
+                  Icons.favorite_border_outlined,
+                  color: Color(0xff5b5858),
+                  size: 50,
                 ),
-                pinned: true,
-                floating: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  title: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    decoration:
-                        BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(10)),
-                    child: provider.loadingState == LoadingState.loading ||
+        ),
+        body: 
+             CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  leading: IconButton(
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Color(0xff5b5858),
+                    ),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                  pinned: true,
+                  floating: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                    title: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      decoration:
+                          BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(10)),
+                      child: provider.loadingState == LoadingState.loading ||
+                              provider.loadingState == LoadingState.idle
+                          ? SizedBox.shrink()
+                          : provider.loadingState == LoadingState.error
+                              ? SizedBox.shrink()
+                              : Text(
+                                  provider.story?.title ?? '',
+                                  style: TextStyle(
+                                      color: Color(0xff5b5858), fontWeight: FontWeight.bold),
+                                ),
+                    ),
+                    background: provider.loadingState == LoadingState.loading ||
                             provider.loadingState == LoadingState.idle
-                        ? SizedBox.shrink()
+                        ? Center(
+                            child: CircularProgressIndicator(
+                              color: Color(0xff5b5858),
+                            ),
+                          )
                         : provider.loadingState == LoadingState.error
-                            ? SizedBox.shrink()
-                            : Text(
-                                provider.story?.title ?? '',
-                                style: TextStyle(
-                                    color: Color(0xff5b5858), fontWeight: FontWeight.bold),
+                            ? Center(child: Text('An error occured!'))
+                            : AspectRatio(
+                                aspectRatio: 1 / 3,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          fit: BoxFit.fitWidth,
+                                          alignment: FractionalOffset.topCenter,
+                                          image: MemoryImage(
+                                              base64Decode(provider.story?.image ?? '')))),
+                                ),
                               ),
                   ),
-                  background: provider.loadingState == LoadingState.loading ||
-                          provider.loadingState == LoadingState.idle
-                      ? Center(
-                          child: CircularProgressIndicator(
-                            color: Color(0xff5b5858),
-                          ),
-                        )
-                      : provider.loadingState == LoadingState.error
-                          ? Center(child: Text('An error occured!'))
-                          : AspectRatio(
-                              aspectRatio: 1 / 3,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                        fit: BoxFit.fitWidth,
-                                        alignment: FractionalOffset.topCenter,
-                                        image: MemoryImage(
-                                            base64Decode(provider.story?.image ?? '')))),
-                              ),
-                            ),
+                  expandedHeight: MediaQuery.of(context).size.height * 0.4,
                 ),
-                expandedHeight: MediaQuery.of(context).size.height * 0.4,
-              ),
-              SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) => bottomContent(provider),
-                  childCount: 1,
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) => bottomContent(provider),
+                    childCount: 1,
+                  ),
                 ),
-              ),
-              SliverToBoxAdapter(
-                child: Container(
-                    height: 50,
-                    decoration: const BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage('res/containerBG.png'), fit: BoxFit.cover))),
-              )
-            ],
-          );
-        },
-      ),
+                SliverToBoxAdapter(
+                  child: Container(
+                      height: 50,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage('res/containerBG.png'), fit: BoxFit.cover))),
+                )
+              ],
+            )
+         
+      );
+      },
+     
     );
   }
 }
